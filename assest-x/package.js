fb42707debd86f2a48ba99660c23feed53d4870a
Aditya <jayaaditya3953@gmail.com>
2025-03-04 20:52:08 +0530

    Update package.js

--- assest-x/package.js
+++ assest-x/package.js
@@ -1,1 +1,19 @@
 
+{
+  "name": "asset-x",
+  "version": "1.0.0",
+  "description": "A simple banking system API",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "bcrypt": "^5.0.1",
+    "cors": "^2.8.5",
+    "express": "^4.17.1",
+    "mongoose": "^5.10.9",
+    "dotenv": "^10.0.0"
+  },
+  "author": "",
+  "license": "ISC"
+}
